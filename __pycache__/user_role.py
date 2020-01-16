a556e3ef64831fa557c453e8a71d6ab0a4188f13
Fabrice-64 <fab.jaouen@gmail.com>
2020-01-16 21:36:28 +0100

    Add Documentation for User module

---
+++ __pycache__/user_role.py
@@ -0,0 +1,23 @@
+""""
+This module deals with the functionalities needed by the User.
+
+They can be described as follows:
+-Get a list of items responding to the selected criteria
+-Record the results in a separate file
+-Add an item to the DB if not already recorded
+
+At any moment the user may search a new item, add another one or quit
+
+"""
+
+#Function to choose the category
+
+#Function to choose the item
+
+#Function to display the results
+
+# Function to record the result in a separate file
+
+# Function to add an item to the database
+
+# Function to select the required functional
