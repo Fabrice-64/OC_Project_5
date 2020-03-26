90584c606505bc1a54dd62f0b6e958516351ee83
Fabrice-64 <fab.jaouen@gmail.com>
2020-03-26 16:30:18 +0100

    Add queries to record and retrieve best product

--- config_queries.py
+++ config_queries.py
@@ -11,4 +11,9 @@ query_searched_item = """SELECT LEFT(product.name,40), product.brand, product.nu
 
 query_best_product = """SELECT LEFT(product.name,40), product.brand, product.nutrition_grade, product.code, product.stores FROM category, product WHERE category.name = \"{}\" AND product.name LIKE \"{}\" AND product.nutrition_grade <= \"{}\" ORDER BY product.nutrition_grade ASC LIMIT 5"""
 
+
 query_retrieve_available_categories = """SELECT DISTINCT category.idcategory, category.name FROM category RIGHT JOIN product ON category.idcategory = product.category_id"""
+
+query_record_best_product = """REPLACE INTO best_product VALUES (NULL, {product_code}, {date_time})"""
+
+query_retrieve_best_product = """SELECT product.name, product.brand, product.stores, product.nutrition_grade, product.code FROM product JOIN best_product ON best_product.product_id = product.code ORDER BY best_product.query_created ASC LIMIT 5"""
