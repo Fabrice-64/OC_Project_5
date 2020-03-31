993e56a9b9c425c7369c082a77400eeedf8209d8
Fabrice-64 <fab.jaouen@gmail.com>
2020-03-31 16:47:47 +0200

    Comments module config_open_food_facts completed

--- config_open_food_facts.py
+++ config_open_food_facts.py
@@ -1,5 +1,14 @@
 """
-This module encompasses all the parameters needed for Open Food Facts API
+    This module encompasses all the parameters needed for Open Food Facts API.
+
+    Class:
+        NIL
+    
+    Exceptions: 
+        NIL
+    
+    Functions:
+        NIL
 """
 
 # Updload data from Open Food Facts
@@ -11,18 +20,19 @@ OFF_PAGE = 1
 
 # The headers are required to get access to the DB. They are defined by OFF administrators.
 HEADERS = {'User-Agent': 'python-requests/2.22.0'}
-# The payload is used in the API to get selected data.
 
+# This payload is used in the API to get selected data.
 PAYLOAD = {'tagtype_0':'categories','tag_contains_0':'contains','tag_0':'','tag_types_1':'countries','tag_contains_1':'contains','tag_0':'fr','json': 1, 'action' : "process", \
     'fields' : "brands,product_name, categories,code,stores,nutrition_grade_fr,ingredients_text","page_size": 1000, "page": OFF_PAGE}
 
 # This url is where the OFF DB is to be found. It is completed by other components, as the payload, the headers, categories, etc.
 URL = 'https://fr.openfoodfacts.org/cgi/search.pl?search_simple=1'
 
-# These are the parameters used to import categories, as static data from OFF
+# These are the parameters used to import categories, as static data from OFF.
 URL_STATIC = 'https://fr.openfoodfacts.org/categories.json'
 STATIC_TAG = 'tags'
 STATIC_FIELD_0 = 'name'
 STATIC_VOLUME = 15
 
+# This is the address to be used to import a product. The product code is added right behind.
 OFF_PRODUCT_ADDRESS = "https://world.openfoodfacts.org/product/"
