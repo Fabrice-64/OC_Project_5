b3ad9c223c47dfed99ec83691e6a14efd7298e5f
Fabrice-64 <fab.jaouen@gmail.com>
2020-05-05 22:20:23 +0200

    Add further Classes to ORM

--- ORM_get_better_diet.py
+++ ORM_get_better_diet.py
@@ -11,7 +11,7 @@ import mysql.connector
 
 class Connection:
     def __init__(self):
-        engine = create_engine("mysql+mysqlconnector://root:@localhost/get_better_diet2",
+        engine = create_engine("mysql+mysqlconnector://root:@localhost/get_better_diet3",
         echo = True, encoding = 'utf-8')
         res = engine.connect()
         res = engine.execute("SHOW TABLES")
@@ -27,11 +27,27 @@ class Category:
     idcategory = Column(Integer(6), primary_key = True, autoincrement = True,
             nullable = False)
     name = Column(String(45), nullable = False)
+    
+
+class Product:
+    __tablename__ = 'product'
+    code = Column(String(13), nullable = False, primary_key = True)
+    brand = Column(String(200), nullable = False)
+    name = Column(String(600), nullable = False)
+    nutrition_grade = Column(String(1), nullable = False)
+
+
+class Store:
+    __tablename__ = 'store'
 
 class CategoryProduct:
     __tablename__ = 'category_product'
-    __table_args__ = ()
+    id_cat_prod = Column(Integer(6), primary_key = True, autoincrement = True,
+        nullable = False)
+    idcategory = Column(Integer(6), ForeignKey('category.idcategory'), nullable = False)
+    code = Column(String(13), nullable = False)
 
-
+class StoreProduct:
+    __tablename__ = 'store_product'
 
 cnx = Connection()
